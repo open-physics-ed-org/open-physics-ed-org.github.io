a1b56d97b25895d8680433d629e76c8e5a760f5f
Danny Caballero <danny.caballero@pm.me>
2025-07-16 16:12:07 -0400

    working on cleaning up code

--- pa11y.py
+++ pa11y.py
@@ -1,6 +1,8 @@
-from oerforge.verify import process_all_html_files
+from oerforge.verify import process_all_html_files, load_pa11y_config
 
-if __name__ == "__main__":
-    config_file = "pa11y-config/pa11y.wcag.aaa.json"
-    db_path = "db/sqlite.db"
-    process_all_html_files(build_dir="build", config_file=config_file, db_path=db_path)
+db_path = "db/sqlite.db"
+config_data = load_pa11y_config("_content.yml")
+wcag_level = config_data.get("wcag_level", "WCAG2AA")
+config_file = config_data.get("config", "pa11y-config/pa11y.wcag.aa.json")
+
+process_all_html_files(build_dir="build", wcag=wcag_level, config_file=config_file, db_path=db_path)
